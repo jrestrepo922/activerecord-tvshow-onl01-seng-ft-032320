f776dc4317820d05c3d653468bd9361adf166e2f
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-26 13:27:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,22 +1,19 @@
 require 'pry'
 class Show < ActiveRecord::Base
+  def highest_rating
+    Show.maximum(:rating)
+    binding.pry
+  end
 
-end
+  def most_popular_show
+     Show.where("rating = ?", highest_rating)
+   end
 
+  def lowest_rating
+    Show.minimum(:rating)
+  end
 
-def highest_rating
-  Show.maximum(:rating)
-  binding.pry
-end
-
-def most_popular_show
-   Show.where("rating = ?", highest_rating)
- end
-
-def lowest_rating
-  Show.minimum(:rating)
-end
-
-def least_popular_show
-  Show.where("rating = ?", lowest_rating)
+  def least_popular_show
+    Show.where("rating = ?", lowest_rating)
+  end
 end
