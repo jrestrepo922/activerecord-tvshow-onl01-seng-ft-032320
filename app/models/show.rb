6417ce68c5340840affa0c0b5a028f51449082ce
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-26 13:28:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,11 +2,12 @@ require 'pry'
 class Show < ActiveRecord::Base
   def highest_rating
     Show.maximum(:rating)
+
   end
 
   def most_popular_show
      Show.where("rating = ?", highest_rating)
-   end
+  end
 
   def lowest_rating
     Show.minimum(:rating)
